966294b52d422be470544149bfe53b49940f6925
SimonWels <wels_freinsheim@web.de>
2024-07-17 11:37:40 +0200

    01Cat.App

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,28 +5,55 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
+  private boolean isFemale;
 
-
-public Cat(String name, String furColor, int age) {
+  private int counter;
+  
+  public Cat(String name, String furColor, int age, boolean isFemale) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
+    this.isFemale = isFemale;
   }
 
-public String getStringAttributes(String op) {
-  switch (op) {
-    case "#name":
-      return name;
-    case "#color":
-      return furColor;
-    default:
-      return "ERROR!";
+  public String getStringAttributes(String op) {
+    switch (op) {
+      case "#name":
+        return name;
+      case "#color":
+        return furColor;
+      default:
+        return "ERROR!";
+    }
   }
-}
 
-public String getAge() {
-    return String.valueOf(age);
+
+  public String getAge() {
+      if (!isFemale) {
+        return String.valueOf(age);
+      } else {
+        return checkCompliance();
+      }
   }
+
+  private String checkCompliance(){
+
+    counter++;
+
+    switch (counter) {
+      case 1:
+        return "This is an inappropriate question!"; // 1st time
+      case 2:
+        return "I've told you once!"; // 2nd time
+      case 3:
+        return "Talk to the hand!"; // 3rd time
+      default:
+        return "1#!?&&%"; //4th ++
+    }
+
+  }
+
+
 
 
 }
