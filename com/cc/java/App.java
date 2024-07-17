d5f860c0faa4c87a3870124613de2ed0ccf7ce59
SimonWels <wels_freinsheim@web.de>
2024-07-17 12:14:27 +0200

    02,Cat.App

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Grizabella", "white", 29, true);
+        Cat cat = new Cat("Grizabella", "white", 29);
 
         output(cat.getStringAttributes("#name"));
         output(cat.getStringAttributes("#color"));
@@ -16,7 +16,7 @@ public class App {
 
         output("-------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
+        Tomcat Tomcat = new Tomcat("Alonzo", "grey",35);
 
         output(cat2.getStringAttributes("#name"));
         output(cat2.getStringAttributes("#color"));
